01572beb02d0d5df9bdc94633da066a71db5feea
CyberErnest <walkamillz20@gmail.com>
2023-06-20 19:56:07 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,22 @@
  * Retrn: 1 is greater than zero, 0 is zero
  * -1 is less than zero
  */
-int print_sign (int n)
+int print_sig (int n)
 {
 	if (n > 0)
-        {
+{
 		_putchar (43);
 		return (1);
-	}
+}
         else if (n < 0)
-        {
+{
 		_putchar (45);
 		return (-1);
-	}
+}
         else
-        {
+{
 		_putchar (48);
 		return (0);
-	}
+}
         _putchar('\n');
 }
